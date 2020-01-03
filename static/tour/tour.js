1990fa59e63f04c15183c998fdda0cc7baf3bc1a
Ravi Pradhan <ravipradhan@protonmail.com>
2020-01-03 17:33:59 +0000

    added fetch code to run when 'Try it out' button is clicked

--- static/tour/tour.js
+++ static/tour/tour.js
@@ -1,27 +1,36 @@
 import { toggleCodeExample } from './utils/CodeExamples'
-import { Fetcher } from './utils/FetchData'
 
-// Event listeners
+// "Call To Action links" 
 const jsExample = document.getElementById('jsExampleLink')
-const callToAction = document.getElementById('tryitout-get-observation')
+const tryItOut = document.getElementById('tryitout-get-observation')
+
+// Results 
+let results;
 const datasetResults = document.getElementById('tryitout-list-datasets-results')
 const jsonResults = document.getElementById('tryitout-list-datasets-json-result')
 
 // Consts
-const endpoint = `https://api.beta.ons.gov.uk/v1${jsExample.dataset.endpoint}`
-const currentPage = callToAction.dataset.page
+const url = `https://api.beta.ons.gov.uk/v1${jsExample.dataset.endpoint}`
+const currentPage = tryItOut.dataset.page
 
 // Event listeners
-jsExample.addEventListener('click', function() {
-    toggleCodeExample(endpoint)
+jsExample.addEventListener('click', function () {
+    toggleCodeExample(url)
 })
 
-callToAction.addEventListener('click', function () {
-    Fetcher.get(endpoint)
-        .then(response => {
-            console.log(response)
-        })
-        .catch(err => {
-            console.error(err)
-        })
+tryItOut.addEventListener('click', function () {
+    tryItOut.setAttribute('disabled', 'true')
+    tryItOut.classList.add('btn--primary-disabled')
+    fetch(url).then(response => {
+        return response.json();
+    }).then(parsedResponse => {
+        results = parsedResponse;
+        datasetResults.classList.remove('hidden')
+    }).catch(err => {
+        throw new Error(err)
+    })
 })
+
+// Event Listener for toggling radio (results/json)
+
+// Functionality for displaying JSON and building a table of results
