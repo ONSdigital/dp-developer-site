3d714d4bb523bbf3268eeaf6ab54d878b6cce5da
Ravi Pradhan <ravipradhan@protonmail.com>
2020-01-08 17:48:07 +0000

    moved previous/next initialisation outside of switch

--- js/utils/tabs.js
+++ js/utils/tabs.js
@@ -12,16 +12,17 @@ const initialiseTabFunctionality = () => {
   });
 
   tabList.addEventListener('keyup', (e) => {
+    let previous = [...tabs].indexOf(activeTab) - 1;
+    let next = [...tabs].indexOf(activeTab) + 1;
+
     switch (e.keyCode) {
       case 37:
         e.preventDefault();
-        let previous = [...tabs].indexOf(activeTab) - 1;
         previous = previous >= 0 ? previous : tabs.length - 1;
         setActiveTab(tabs[previous].getAttribute('aria-controls'));
         break;
       case 39:
         e.preventDefault();
-        let next = [...tabs].indexOf(activeTab) + 1;
         next = next < tabs.length ? next : 0;
         setActiveTab(tabs[next].getAttribute('aria-controls'));
         break;
