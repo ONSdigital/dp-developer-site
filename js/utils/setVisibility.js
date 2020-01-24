519f4d1caa5b5a7d38cc34e2669d25dac2b5c0b5
Ravi Pradhan <ravipradhan@protonmail.com>
2020-01-24 11:31:15 +0000

    removed console logs

--- js/utils/setVisibility.js
+++ js/utils/setVisibility.js
@@ -1,9 +1,7 @@
 const setVisibility = (el, isVisible) => {
   if (isVisible) {
-    console.log('visible')
     el.classList.remove('hidden');
   } else {
-    console.log('hidden')
     el.classList.add('hidden');
   }
 };
