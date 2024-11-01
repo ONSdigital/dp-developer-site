68a92e341b326fd5cfe74f19532f951040a5cc70
janderson2 <janderson2@users.noreply.github.com>
2024-11-01 14:15:32 +0000

    Improve function names and add static dir constant
    Improve the function names to make it clear that the "generate"
    functions were not actually generating the static site HTML pages. Also
    add a constant for the static page content directory rather than having
    a string literal buried in the codebase.

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import (
 )
 
 const (
-	serviceName = "dp-developer-site"
+	serviceName   = "dp-developer-site"
+	staticRootDir = "static"
 )
 
 func main() {
@@ -37,7 +38,7 @@ func main() {
 
 	// Generate site model
 	orderedNav := createNav()
-	siteModel := site.NewSite(orderedNav, sources)
+	siteModel := site.NewSite(orderedNav, sources, staticRootDir)
 
 	// Generate static HTML site
 	log.Info(ctx, "creating assets directories and HTML files")
